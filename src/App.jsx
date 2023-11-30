423e00ce1d88d19d0ea0d479df48545fca17b893
Muneer-Raza Attari <131713767+MuneerRaza19@users.noreply.github.com>
2023-11-30 20:15:38 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import MortgageCalculator from './MortgageCalculator';
 function App() {
   return (
     <div>
-      <h1>Your App Title</h1>
+      <h1>Muneer's Mortgage Calculator </h1>
       <MortgageCalculator />
     </div>
   );
